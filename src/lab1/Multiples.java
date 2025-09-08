3d34bfe4931df0a9da105df36c1ced129fc0b84c
Joyi <joyi.x.06@gmail.com>
2025-09-08 12:45:38 -0400

    idk made them public

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -5,11 +5,11 @@ public class Multiples {
         int count = multiples(1000, 3, 5);
         System.out.println(count);
     }
-    static int multiples(){
+    public static int multiples(){
         return multiples(1000, 3, 5);
 
     }
-    static int multiples(int n, int a, int b) {
+    public static int multiples(int n, int a, int b) {
         int c = 0;
         for (int i = 1; i < n; i++){
             if (i % a == 0 || i % b == 0){
